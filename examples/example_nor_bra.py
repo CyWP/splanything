ba665354ac51cdff89d4a608d04d3e1737c7383a
Thomas Peschlow <thomas.p24@live.com>
2026-08-23 07:21:19 -0400

    Splimage fixes

--- examples/example_nor_bra.py
+++ examples/example_nor_bra.py
@@ -73,19 +73,19 @@ def get_primitive():
     # cubic = CubicFanPrimitive(size=10).to(device)
     msk = Splimage("../assets/bra_nor_offside_masked.png", mask_mode="A", as_mask=True)
     radial = RadialFreqPrimitive(
-        size=1500,
+        size=80,
         initializers={
             "thetas": ThetaZero(),
-            "centroids": MappedInitializer(msk.blur(40) * 0.5 + 0.1),
+            "centroids": MappedInitializer(msk.blur(10)),
         },
         param_defs={"thetas": ParamDef(batched=True, trainable=False)},
     ).to(device)
     star = StarPrimitive(
-        size=800,
+        size=80,
         n_axes=2,
         initializers={
             "thetas": ThetaZero(),
-            "centroids": MappedInitializer(msk.blur(100) * 0.3 + 0.2),
+            "centroids": MappedInitializer(msk.blur(100) * 0.3 + 0.05),
         },
         param_defs={"thetas": ParamDef(batched=True, trainable=False)},
     ).to(device)
@@ -107,12 +107,12 @@ def train():
 
     # Rules
     alpha_cull = ThresholdFilter(attr_name="alphas", threshold=0.1, interval=17)
-    grad_split_lo = GradSplit(threshold=0.0005, interval=201, attr_names=["centroids"])
+    grad_split_lo = GradSplit(threshold=0.005, interval=201, attr_names=["centroids"])
     grad_split_hi = GradSplit(threshold=0.02, interval=173, attr_names=["centroids"])
     area_split = ThresholdFilter(attr_name="areas", threshold=0.1, interval=87)
     area_filter = ThresholdFilter(attr_name="areas", threshold=2e-4, interval=69)
     map_split = MapSplit(msk_blur10 * 0.1 + 0.02, interval=307)
-    ceiling = PrimitiveCeiling(10000)
+    ceiling = PrimitiveCeiling(2500)
     prim.add_split_rule(map_split)
     prim.add_filter_rule(alpha_cull)
     prim.add_filter_rule(area_filter)
@@ -148,7 +148,7 @@ def train():
     )
     train_callbacks = [
         PreviewWindow(
-            frequency=5,
+            frequency=3,
             show_target=False,
             sampler=vis_sampler,
             save_folder=run_folder / "train_preview",
@@ -170,7 +170,9 @@ def train():
         AttributeProximity(["color_1", "color_2"], mode="PUSH"),
         weight=0.05,
     )
-    prim.add_regularizer("Area Range", AttributeRange("areas", min=0.001), weight=12.0)
+    prim.add_regularizer(
+        "Area Range", AttributeRange("areas", min=0.0025), weight=100.0
+    )
     prim.add_regularizer(
         "Alpha Target", AttributeRange("alphas", min=0.95), weight=12.0
     )
@@ -184,22 +186,6 @@ def train():
     #     AttributeProximity(["scales_1", "scales_2"], mode="RATIO", ratio=0.5),
     #     weight=5.0,
     # )
-    prim["radial"].add_regularizer(
-        "Dark Test 1",
-        AttributeRange("color_1", target=0.0, weight_map=msk_blur10),
-        weight=1000000,
-    )
-    prim["radial"].add_regularizer(
-        "Dark Test 2",
-        AttributeRange("color_2", target=0.0, weight_map=msk_blur10),
-        weight=1000000,
-    )
-
-    prim["star"].add_regularizer(
-        "Dark Test 3",
-        AttributeRange("color", target=0.0, weight_map=msk_blur10),
-        weight=1000000,
-    )
 
     # Trainer
     trainer = Trainer(
@@ -221,18 +207,20 @@ def generate():
     # Load primitive
     gen_H = 3072
     gen_W = 1024
-    gen_padding = (1024, 1536, 1536, 1536)
+    gen_padding = (1536, 1024, 1024, 1024)
     prim = get_primitive()
     prim.load(run_folder / "primitive.pt")
     prim.requires_grad_(False)
     prim = prim.to(device)
-    msk = Splimage(
-        "../assets/bra_nor_offside_masked.png", mask_mode="A", as_mask=True
-    ).resize(gen_H, gen_W)
+    msk = (
+        Splimage("../assets/bra_nor_offside_masked.png", mask_mode="A", as_mask=True)
+        .to(device)
+        .resize(gen_H, gen_W)
+    )
 
     # Sample processor
     sample_proc = FlexibleSampleProcessor(
-        lambda s, p: SampleOutput(s.rgb, s.weights, s.co)
+        lambda s, p: SampleOutput(s.rgb, s.weights**1.08, s.co)
     )
     dist_proc = DistanceSampleProcessor(
         sample_proc,
@@ -240,24 +228,35 @@ def generate():
             s.rgb, s.weights * (torch.sin(d * 10000) * 0.2 + 0.8), s.co
         ),
     )
-    vec_proc = VecSampleProcessor(
+    diag_proc = VecSampleProcessor(
         sample_proc,
         lambda s, p, x, y: SampleOutput(
-            s.rgb, s.weights * torch.sin(y * 7000) ** 2, s.co
+            s.rgb,
+            s.weights * (torch.cos(x * 1000) * torch.sin(y * 7000) * 0.5 + 0.5) ** 2,
+            s.co,
+        ),
+    )
+    vert_proc = VecSampleProcessor(
+        sample_proc,
+        lambda s, p, x, y: SampleOutput(
+            s.rgb,
+            s.weights * (torch.sin(y * 7000) * 0.5 + 0.5) ** 2,
+            s.co,
         ),
     )
     multi_proc = MultiSampleProcessor(
         [
             (sample_proc, msk.blur(40)),
             (dist_proc, 1 - msk.blur(40)),
-            (vec_proc, 3),
+            (diag_proc, msk.blur(100) * 3),
+            (vert_proc, (1 - (msk.blur(100))) * 3),
         ],
         normalize_weights=True,
     )
     prim.add_sample_processor(multi_proc)
     color_proc = ColorSkewSampleProcessor(
         torch.tensor([[1.0, 0.5, 0.0], [1.0, 0.9, 0.6], [0.0, 0.0, 0.0]]).to(device),
-        sigma=0.5,
+        sigma=3.0,
         reduction="MIN",
         rescale=True,
     )
@@ -266,8 +265,8 @@ def generate():
     # Rasterizer
     rast = MultiRasterizer(
         [
-            (WeightedRasterizer(), msk.blur(100)),
-            (ProbabilisticRasterizer(top_k=5), 1 - msk.blur(100)),
+            (WeightedRasterizer(), msk.blur(700)),
+            (ProbabilisticRasterizer(top_k=5), 1 - msk.blur(700)),
         ]
     )
 
